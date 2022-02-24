1e13d9d6f02f49b3075f01266dfcb7ca270d7e90
Torkil Rogneflaaten <Torrog99@gmail.com>
2022-02-24 20:50:06 +0100

    filtering out the two and four first minutes of every bout and saving them as separate files, "session-data-6min" and "session-data-4min" respectivly.

--- docs/session-data-import.R
+++ docs/session-data-import.R
@@ -4,6 +4,7 @@
 ## Loading needed packages
 library(tidyverse)
 library(readxl)
+library(lubridate)
 
 # List all files in the data folder
 files <- list.files("data/session-data/")
@@ -21,9 +22,29 @@ for(i in 1:length(files)) {
 
 # Bind all files together
 dat <- bind_rows(file.list)
+dat
 
 
 ## Save imported data 
 saveRDS(dat, "data/derivedData/session-data.RDS")
 
+
+# filter out the 2 first mins of every bout
+# for some reason i had to write one hour higher than intended...
+# maybe the time-zone for the date variable [time] is different from the time-zone on my computer?
+dat_6min <- dat %>% 
+  filter(time >= "1899-12-31 03:00:00", 
+         time <= "1899-12-31 09:00:00")
+
+## save filtered (6min) imported data
+saveRDS(dat_6min, "data/derivedData/session-data-6min.RDS")
+
+
+# filter out the 4 first mins of every bout
+dat_4min <- dat %>% 
+  filter(time >= "1899-12-31 05:00:00", 
+         time <= "1899-12-31 09:00:00")
+
+## save filtered (4min) imported data
+saveRDS(dat_4min, "data/derivedData/session-data-4min.RDS")
 
